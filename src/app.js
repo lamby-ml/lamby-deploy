5f2e735329b4a5f9496363309f4dd09673bf2903
Charlie Crouse <charlietcrouse@gmail.com>
2019-04-09 17:06:17 -0400

    Add support for gzipped onnx models

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+import http from 'http';
 import os from 'os';
+import zlib from 'zlib';
+
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
@@ -30,21 +33,19 @@ const registerRoutes = app => {
   app.get('/', async (req, res) => {
     const message = `
       <h1>Your API is up and running successfully!</h1>
-      <h3>Here is an example on how to use your model to predict values</h3>
+      <h3>Here is an example on how to use your model to predict values using squeezenet</h3>
       <p>
         curl -H "Content-Type: application/json" -X POST
-          -d '{ "values": [1.0,2.0,3.0,4.0] }' http://${os.hostname()}/predict
+          -d '{ "values": [1.0, ..., 2.0], "dim": [1, 3, 224, 224] }' http://${os.hostname()}/predict
       </p>`;
     res.send(message);
   });
 
   app.post('/predict', async (req, res) => {
     try {
-      const { values } = req.body;
+      const { values, dim } = req.body;
 
-      const dim = Math.floor(Math.sqrt(values.length));
-
-      const inputs = [new Tensor(new Float32Array(values), 'float32', [dim, dim])];
+      const inputs = [new Tensor(new Float32Array(values), 'float32', dim)];
 
       const outputMap = await app.locals.session.run(inputs);
       const outputTensor = outputMap.values().next().value;
@@ -58,11 +59,35 @@ const registerRoutes = app => {
   return app;
 };
 
+const downloadAndUnzip = async url => {
+  const buffer = [];
+
+  http.get(url, res => {
+    const gunzip = zlib.createGunzip();
+    res.pipe(gunzip);
+
+    gunzip
+      .on('data', data => {
+        buffer.push(data.toString());
+      })
+      .on('end', () => {
+        return buffer.join('');
+      })
+      .on('error', error => {
+        throw error;
+      });
+  });
+};
+
 const createSession = async () => {
   const session = new InferenceSession();
-  const url = process.env.ONNX_MODEL_URI || './src/data/model.onnx';
 
-  await session.loadModel(url);
+  if (process.env.ONNX_MODEL_URI !== undefined) {
+    const modelBuffer = await downloadAndUnzip(process.env.ONNX_MODEL_URI);
+    await session.loadModel(modelBuffer);
+  } else {
+    await session.loadModel('./src/data/model.onnx');
+  }
 
   return session;
 };
